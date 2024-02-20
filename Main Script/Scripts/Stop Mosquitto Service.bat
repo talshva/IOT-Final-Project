c33e3ee8014d526bcca1abbab92a0d13ad8812d0
Tal Shvartzberg <82408347+talshva@users.noreply.github.com>
2024-02-20 14:29:41 +0200

    Delete Main Script directory

--- Main Script/Scripts/Stop Mosquitto Service.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-REM Stop Mosquitto service after Python script execution
-sc stop mosquitto
-if %errorlevel% neq 0 (
-    echo Failed to stop Mosquitto service. Ensure you have the necessary permissions.
-)
-pause
